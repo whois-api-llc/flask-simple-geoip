adc44e2ce047a11e8ee8d6efa538c930d3fbe92a
Randall Degges <r@rdegges.com>
2018-06-16 23:10:51 -0700

    First release of the core Flask-Simple-GeoIP library.

---
+++ flask_simple_geoip.py
@@ -0,0 +1,43 @@
+from os import environ
+
+from simple_geoip import GeoIP
+from flask import request
+
+
+class SimpleGeoIP(object):
+    def __init__(self, app=None):
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        if not (app.config.get("GEOIPIFY_API_KEY") or environ.get("GEOIPIFY_API_KEY")):
+            raise Exception("No API key was supplied for performing GeoIP lookups. Please set a value for GEOIPIFY_API_KEY.")
+
+        self.geoip_client = GeoIP(environ.get("GEOIPIFY_API_KEY"))
+
+    def get_geoip_data(self):
+        """
+        Performs a geoip lookup based on the requester's public IP address.
+
+        NOTE: This method will *always* return and never raise an exception --
+        it does this strategically because performing a geoip lookup should
+        never be a tier 1 stop-the-world exception.
+
+        :rtype: dict or None
+        :returns: A dictionary containing the user's geolocation data, or None
+            if there was a problem.
+        """
+        try:
+            data = self.geoip_client.lookup(request.remote_addr)
+
+        # Don't do anything if an exception arises -- since geolocation data isn't
+        # critical to a request being processed, we can always skip it in the worst
+        # case scenario.
+        #
+        # By default, the underlying `simple_geoip` library will handle retry logic
+        # for us ;)
+        except:
+            data = None
+
+        return data
