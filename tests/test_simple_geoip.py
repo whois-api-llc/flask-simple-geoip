9145b06875bcb7e12be085bedd18c009f620b83f
Randall Degges <r@rdegges.com>
2018-06-16 23:51:34 -0700

    Fixing tests with encrypted environ variable.

--- tests/test_simple_geoip.py
+++ tests/test_simple_geoip.py
@@ -12,8 +12,15 @@ class TestSimpleGeoIP(TestCase):
         self.app = Flask(__name__)
 
     def test_init_fails_with_no_config(self):
+        api_key = environ.get("GEOIPIFY_API_KEY")
+        if api_key:
+            del environ["GEOIPIFY_API_KEY"]
+
         with self.assertRaises(Exception):
             SimpleGeoIP(app)
+
+        if api_key:
+            envion["GEOIPIFY_API_KEY"] = api_key
 
     def test_init_works_with_environment_variable_config(self):
         environ["GEOIPIFY_API_KEY"] = "test"
